0ec8e01ee6e176fba9a257d5130118b4e081b459
lovetjr <850354060@qq.com>
2019-02-07 20:31:37 +0800

    毕业设计_v1.0版本

---
+++ src/test/java/com/sport/advisory/AdvisoryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sport.advisory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvisoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
